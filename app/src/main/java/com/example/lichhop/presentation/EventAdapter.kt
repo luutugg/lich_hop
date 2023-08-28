bf9d09b0b015b53aa31064257f049b2252166b59
tunglv <tunglv@ftech.ai>
2023-08-28 10:43:57 +0700

    push

--- app/src/main/java/com/example/lichhop/presentation/EventAdapter.kt
+++ app/src/main/java/com/example/lichhop/presentation/EventAdapter.kt
@@ -1,15 +1,13 @@
 package com.example.lichhop.presentation
 
 import android.annotation.SuppressLint
-import android.util.TypedValue
+import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.PARENT_ID
-import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -48,6 +46,11 @@ class EventAdapter : ListAdapter<CacularEventDisplay, EventAdapter.EventVH>(Even
 
     inner class EventVH(private val binding: EventItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        init {
+
+        }
+
         @SuppressLint("ResourceType")
         fun onBind(data: CacularEventDisplay) {
             with(binding) {
@@ -55,27 +58,28 @@ class EventAdapter : ListAdapter<CacularEventDisplay, EventAdapter.EventVH>(Even
                     text = data.data?.dayName
                     val params = tvEventTime.layoutParams
                     params.width = data.widthTotal!!
-                    params.height = data.heightTotal?.div(20)!!
+                    params.height = (data.heightTotal?.div(20)!!)
                     tvEventTime.layoutParams = params
                 }
 
                 for (i in 0 until data.data?.event?.size!!) {
                     var marginTop = 0
-                    if (i > 0) {
+                    marginTop = if (i > 0) {
                         val distanceEvent = minusTime(
                             data.data?.event?.get(i - 1)?.endTime ?: "",
                             data.data?.event?.get(i)?.startTime ?: ""
                         )
-                        marginTop = data.heightTotal?.div(20)!! * distanceEvent
+                        data.heightTotal?.div(20)!! * distanceEvent
                     } else {
                         val distanceEvent =
                             minusTime("08:00:00", data.data?.event?.get(i)?.startTime ?: "")
-                        marginTop = data.heightTotal?.div(20)!! * distanceEvent
+                        data.heightTotal?.div(20)!! * distanceEvent
                     }
 
                     val tv = TextView(this.root.context)
                     tv.apply {
-                        text = "${ data.data?.event?.get(i)?.userName }\n${data.data?.event?.get(i)?.content}"
+                        text =
+                            "${data.data?.event?.get(i)?.userName}\n${data.data?.event?.get(i)?.content}"
                         setTextColor(getAppColor(R.color.white))
                         gravity = Gravity.CENTER
                         setBackgroundColor(getAppColor(R.color._test))
@@ -88,10 +92,15 @@ class EventAdapter : ListAdapter<CacularEventDisplay, EventAdapter.EventVH>(Even
                     val params = tv.layoutParams as ViewGroup.MarginLayoutParams
                     params.width = data.widthTotal ?: 0
                     params.height = data.heightTotal?.div(20)!! * part
-                    params.topMargin = marginTop + 1
+                    params.topMargin = marginTop + i
                     tv.layoutParams = params
                 }
             }
+        }
+
+        private fun Int.convertDpToPixels(): Int {
+            val metrics: DisplayMetrics = getApplication().getResources().getDisplayMetrics()
+            return (this * (metrics.densityDpi / 160f)).toInt()
         }
 
         private fun minusTime(startTime: String, endTime: String): Int {
